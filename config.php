9adc2827af00eb5e9d982555ceaaf871ecdff63d
Emiwodew <mickeynig@gmail.com>
2016-03-11 18:16:31 +0100

    Configuration Pages

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//Database Constants
+defined("DB_SERVER") ? null: define("DB_SERVER", "localhost");
+defined("DB_USER") ? null: define("DB_USER", "dbwa_macatrons");
+defined("DB_PASS") ? null: define("DB_PASS", "M7u7JASY");
+defined("DB_NAME") ? null: define("DB_NAME", "dbwa_macatrons");
+?>
---
+++ header.php
@@ -0,0 +1,9 @@
+<?php ?>
+<html lang="en">
+<head>
+    <title>Refugee Support Network</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+</head>
+<body>
